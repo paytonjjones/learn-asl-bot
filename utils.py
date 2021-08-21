5a39c5de8079e3585010c6521f6eec27458f201c
payton-jones <payton-jones@pluralsight.com>
2021-08-21 19:07:21 +0000

    set to run in basic loop

---
+++ utils.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import requests
+import pickle
+import re
+import random
+import praw
+import time
+from bs4 import BeautifulSoup
+
+def parse_entire_dictionary():
+    stem = 'https://lifeprint.com/asl101/index/'
+    full_dict = {}
+    for letter in 'abcdefghijklmnopqrstuvwxyz': #
+        letter_dict = parse_dictionary_main_page(stem + letter + '.htm')
+        full_dict.update(letter_dict)
+        time.sleep(0.5)
+    return full_dict
+
+def parse_dictionary_main_page(url):
+    '''
+    The dictionary main page contains links to subpages with words.
+    Takes a url (ex. https://lifeprint.com/asl101/index/a.htm)
+    Returns a dictionary with the dictionary entry as the key,
+    and the link to the dictionary content page as the value
+    '''
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    links_html = soup.find_all("a", href = True)
+    links_html = [link for link in links_html if len(link.parent) > 1]
+    master_dict = {}
+    for blob in links_html:
+        text = blob.text
+        link = blob.get("href")
+        link = re.sub('\.\.', 'https://lifeprint.com/asl101/', link)
+        master_dict[text] = link
+    return(master_dict)
+
+def parse_dictionary_content_page(url, name):
+    '''
+    The content page contains images and videos describing the word
+    Takes a url (ex. https://lifeprint.com/asl101//pages-signs/a/active.htm)
+    Returns a pandas dataframe as follows:
+    # pandas df:
+    # name
+    # type ("image", "gif", or "video")
+    # text
+    # location (the web address where the content can be found)
+    '''
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    videos = soup.find_all('iframe')
+    df = pd.DataFrame()
+    for video in videos:
+        row = {}
+        row['name'] = name
+        row['type'] = 'youtube'
+        row['text'] = get_description(video)
+        row['location'] = video['src']
+        df = df.append(row, ignore_index=True)
+    return(df)
+
+def get_description(element):
+    text_array = []
+    temp = element
+    while True:
+        temp = temp.previous
+        if temp.name == 'hr' or temp.name == 'font':
+            break 
+        if isinstance(temp, type('bs4.element.NavigableString')):
+            text_array.append(temp)
+    # reverse text_array, then
+    text_array.reverse()
+    description = ''.join(text_array)
+    # Clean for Reddit Title 
+    description = re.sub('(\n|\t)+', '', description)
+    description = smart_truncate(description)
+    return(description)
+
+def smart_truncate(content, length=300, suffix='...'):
+    # from https://stackoverflow.com/questions/250357/truncate-a-string-without-ending-in-the-middle-of-a-word
+    if len(content) <= length:
+        return content
+    else:
+        return ' '.join(content[:length+1].split(' ')[0:-1]) + suffix
+    
+def post_random_content(main_page, reddit_creds, content_type = 'random'):
+    if content_type == 'random':
+        content_type = random.choice(['image', 'youtube', 'mp4'])
+    content_df = pd.DataFrame
+    i = 0
+    while True:
+        name = random.choice([*main_page.keys()])
+        url = main_page[name]
+        content_df = parse_dictionary_content_page(url, name)
+        i += 1
+        if i > 100 or not content_df.empty:
+            break
+    chosen_row = content_df.iloc[random.choice(content_df.index)]
+    if content_type == 'image':
+        pass
+    elif content_type == 'youtube':
+        post_youtube(chosen_row, reddit_creds)
+    elif content_type == 'mp4':
+        pass
+    return chosen_row
+
+def post_youtube(df_row, reddit_creds):
+    title = df_row['name'] + ' | ' + df_row['text']
+    url = df_row['location']
+    reddit = praw.Reddit(
+        user_agent="test",
+        client_id=reddit_creds['CLIENT_ID'],
+        client_secret=reddit_creds['CLIENT_SECRET'],
+        username=reddit_creds['USERNAME'],
+        password=reddit_creds['PASSWORD'])
+    reddit.validate_on_submit = True
+    reddit.subreddit("learnASL").submit(title=title, url=url)
+    print('YouTube embedded video posted to Reddit')
